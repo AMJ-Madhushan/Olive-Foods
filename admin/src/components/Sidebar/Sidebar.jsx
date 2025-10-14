44f8f7c73409c129a783da0c427d41bb78c8919f
Deneth Pinsara <it21158186@my.sliit.lk>
2025-10-15 02:44:10 +0530

    feat: enhance Sidebar with headers and tooltips for better navigation

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -6,18 +6,24 @@ import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
+      <div className="sidebar-header">
+        <h3>Admin Panel</h3>
+      </div>
       <div className="sidebar-options">
         <NavLink to='add' className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
+          <img src={assets.add_icon} alt="Add Items" />
           <p>Add Items</p>
+          <div className="tooltip">Add new food items to the menu</div>
         </NavLink>
         <NavLink to='list' className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+          <img src={assets.order_icon} alt="List Items" />
           <p>List Items</p>
+          <div className="tooltip">Manage existing food items</div>
         </NavLink>
         <NavLink to='orders' className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+          <img src={assets.order_icon} alt="Orders" />
           <p>Orders</p>
+          <div className="tooltip">View and manage customer orders</div>
         </NavLink>
       </div>
     </div>
